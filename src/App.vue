a5640f335ccb7fe20f485403d5a08d8264288ada
贠乾 <yunqian8@live.com>
2019-10-27 22:21:21 +0800

    新增注解

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div>
-    <router-view/>
+    <router-view/>//这里就是 路由子组件容器
   </div>
 </template>
 
