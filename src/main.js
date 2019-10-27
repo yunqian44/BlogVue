a108946aff6c65ea186c90ab448b4f98870c4638
贠乾 <yunqian8@live.com>
2019-10-27 22:21:45 +0800

    新增注解

--- src/main.js
+++ src/main.js
@@ -1,10 +1,10 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue'//导入vue
+import App from './App.vue'//导入 app.vue 主组件
 import './registerServiceWorker'
-import router from './router'
+import router from './router'//导入路由  也可以写 router.jssss
 
 Vue.config.productionTip = false
-
+//将上边的全局变量赋给 vue 实例化，并挂载到 #app上
 new Vue({
   router,
   render: h => h(App)
