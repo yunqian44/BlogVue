2621d59987d43e869139d5ec54f3b40c2dd40b76
贠乾 <yunqian8@live.com>
2019-11-04 17:37:23 +0800

    新增Vuex的使用 挂在Vuex 新增日期转化方法 修改网络请求Http.js  (加入状态码判断)

--- src/main.js
+++ src/main.js
@@ -1,7 +1,13 @@
 import Vue from 'vue'//导入vue
 import App from './App.vue'//导入 app.vue 主组件
+import store from "./store";
 import './registerServiceWorker'
-import router from './router'//导入路由  也可以写 router.jssss
+import router from './router'//导入路由  也可以写 router.js
+
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+Vue.use(ElementUI)
+
 
 // 引用API文件
 import api from './api/http.js'
@@ -11,6 +17,7 @@ Vue.prototype.$api = api
 Vue.config.productionTip = false
 //将上边的全局变量赋给 vue 实例化，并挂载到 #app上
 new Vue({
+  store,
   router,
   render: h => h(App)
 }).$mount('#app')
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,12 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'// 引用路由
 import Home from '../views/Home.vue'//导入方法1 Home页面
+import FormVuex from "../views/FormVuex.vue";
+import Login from "../views/Login";
+import store from "../store";
+import Editor from "../views/Editor";
+import Content from "../views/Content";
+import Home2 from "../views/Home2";
 
 Vue.use(VueRouter)
 
@@ -9,6 +15,34 @@ const routes = [
     path: '/',
     name: 'home',
     component: Home
+  },
+  {
+    path: '/home2',
+    name: 'home2',
+    component: Home2
+  },
+  {
+    path: "/Content/:id",
+    name: "Content",
+    component: Content,
+    meta: {
+      requireAuth: true // 添加该字段，表示进入这个路由是需要登录的
+    }
+  },
+  {
+    path: "/Editor",
+    name: "Editor",
+    component: Editor
+  },
+  {
+    path: "/Login",
+    name: "Login",
+    component: Login
+  },
+  {
+    path: "/Vuex",
+    name: "Vuex",
+    component: FormVuex
   },
   {
     path: '/about',//路径
@@ -22,5 +56,30 @@ const router = new VueRouter({
   base: process.env.BASE_URL,
   routes
 })
+
+var storeTemp = store;
+router.beforeEach((to, from, next) => {
+  if (!storeTemp.state.token) {
+    storeTemp.commit("saveToken", window.localStorage.Token);
+  }
+  if (to.meta.requireAuth) {
+    // 判断该路由是否需要登录权限
+    if (storeTemp.state.token) {
+      // 通过vuex state获取当前的token是否存在
+      next();
+    } else {
+      //这里使用Id4授权认证，用Jwt，请删之，并把下边的跳转login 打开；
+      // applicationUserManager.login();
+
+      //这里使用Jwt登录，如果不用Id4授权认证，这里打开它；
+      next({
+        path: "/login",
+        query: { redirect: to.fullPath } // 将跳转的路由path作为参数，登录成功后跳转到该路由
+      });
+    }
+  } else {
+    next();
+  }
+});
 
 export default router
