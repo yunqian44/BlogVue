78ded5a3b9c43e13287a4d5c290660583f1d3c2e
贠乾 <yunqian8@live.com>
2020-01-13 18:38:34 +0800

    修改ipd 验证相关信息

--- Auth/applicationusermanager.js
+++ Auth/applicationusermanager.js
@@ -3,12 +3,12 @@ import { UserManager } from 'oidc-client'
 class ApplicationUserManager extends UserManager {
   constructor () {
     super({
-      authority: 'http://localhost:5002',
+      authority: 'http://localhost:7005',
       client_id: 'blogvuejs',
-      redirect_uri: 'http://localhost:6688/callback',
+      redirect_uri: 'http://localhost:7002/callback',
       response_type: 'id_token token',
       scope: 'openid profile roles blog.core.api',
-      post_logout_redirect_uri: 'http://localhost:6688'
+      post_logout_redirect_uri: 'http://localhost:7002'
     })
   }
 
